43a4575389e2c3ff0ebcea5bf31965d5c7c1ad7d
jonathanhamler <jkhamler@gmail.com>
2018-12-13 14:05:43 +0000

    UI Tweaks

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,16 +11,9 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-<<<<<<< HEAD
-public function boot()
-{
-    Schema::defaultStringLength(191);
-}
-=======
     public function boot()
     {
     }
->>>>>>> a7df634f59bf91a8f6b16434906a2afee94fd512
 
     /**
      * Register any application services.
